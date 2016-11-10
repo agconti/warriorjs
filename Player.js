df5ae40e4bd3925b420457799da1d4ef7255169f
Conti <agc11d@gmail.com>
2016-11-10 14:27:54 -0500

    wip

--- Player.js
+++ Player.js
@@ -1,5 +1,25 @@
 class Player {
+  constructor() {
+    this.directions = ['forward', 'left', 'right', 'backward']
+  }
+  checkSurroundings(warrior) {
+    const surroundingsReducer = (acc, direction) => acc[direction] = warrior.feel(direction)
+    const surroundings = this.directions.reduce(surroundingsReducer, new Set())
+
+    return surroundings
+  }
+  act(space){
+    switch space {
+      case space.isEmpty():
+        return warrior => warrior.walk()
+    }
+
+  }
   playTurn(warrior) {
-    warrior.walk(warrior.directionOfStairs())
+    const surroundings = checkSurroundings(warrior)
+    const surroundings.prioritize()
+
+    const actions = surroundings.map(this.act)
+    actions.map(action => action(warrior))
   }
 }
